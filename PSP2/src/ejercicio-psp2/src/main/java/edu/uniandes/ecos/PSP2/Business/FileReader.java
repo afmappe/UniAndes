36d3cfb2334dafbf4e1d0b8acd4073fed47eb5f2
Andres <afmappe@hotmail.com>
2017-03-03 17:01:32 -0500

    Ajuste de pruebas

--- PSP2/src/ejercicio-psp2/src/main/java/edu/uniandes/ecos/PSP2/Business/FileReader.java
+++ PSP2/src/ejercicio-psp2/src/main/java/edu/uniandes/ecos/PSP2/Business/FileReader.java
@@ -1,13 +1,9 @@
 package edu.uniandes.ecos.PSP2.Business;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
-import org.junit.Assert;
 
 public class FileReader {
 
@@ -20,19 +16,6 @@ public class FileReader {
 		byte[] data = Files.readAllBytes(path);
 		String content = new String(data);
 		return content;
-	}
-
-	public static Path getResourcePath(String resourceName) {
-		Path path = null;
-		try {
-			ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-			URL url = classLoader.getResource(resourceName);
-			path = Paths.get(url.toURI());
-		} catch (URISyntaxException e) {
-			Assert.fail("Test Fail");
-			e.printStackTrace();
-		}
-		return path;
 	}
 
 }
--- PSP2/src/ejercicio-psp2/src/test/java/edu/uniandes/ecos/PSP2/TestCase.java
+++ PSP2/src/ejercicio-psp2/src/test/java/edu/uniandes/ecos/PSP2/TestCase.java
@@ -1,19 +1,26 @@
 package edu.uniandes.ecos.PSP2;
 
-import java.io.IOException;
+import static org.junit.Assert.*;
+
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.DecimalFormat;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import edu.uniandes.ecos.PSP2.App.Controller;
-import edu.uniandes.ecos.PSP2.Business.FileReader;
-import edu.uniandes.ecos.PSP2.Business.Regression;
 import edu.uniandes.ecos.PSP2.Entities.DataInfo;
 import edu.uniandes.ecos.PSP2.Interfaces.IController;
 
 public class TestCase {
 
 	private final String FILE_TEST1 = "Test1/TestData.csv";
+	private final String FILE_TEST2 = "Test2/TestData.csv";
+
+	private final double PRECISION = 0.01;
 
 	private IController controller;
 
@@ -22,14 +29,46 @@ public class TestCase {
 	}
 
 	@Test
-	public void Test1() {
+	public void Values() {
 		try {
-			DataInfo data = controller.execute(FILE_TEST1);
-			Assert.assertNotNull("", data);
+
+			Path path = getResourcePath(FILE_TEST1);
+			DataInfo data = controller.execute(path.toString());
+
+			assertNotNull("Is null", data);
+			assertEquals("Value does not match", -22.55, data.getParameterB0(), PRECISION);
+			assertEquals("Value does not match", 1.7279, data.getParameterB1(), PRECISION);
+			assertEquals("Value does not match", 0.9545, data.getCorrelationCoefficient(), PRECISION);
+			assertEquals("Value does not match", 0.9111, data.getR2(), PRECISION);
+			assertEquals("Value does not match", 644.429, data.getYk(), PRECISION);
+
+			path = getResourcePath(FILE_TEST2);
+			data = controller.execute(path.toString());
+			
+			assertNotNull("Is null", data);
+			assertEquals("Value does not match", -4.039, data.getParameterB0(), PRECISION);
+			assertEquals("Value does not match", 0.1681, data.getParameterB1(), PRECISION);
+			assertEquals("Value does not match", 0.9333, data.getCorrelationCoefficient(), PRECISION);
+			assertEquals("Value does not match", 0.8711, data.getR2(), PRECISION);
+			assertEquals("Value does not match", 60.858, data.getYk(), PRECISION);
+
 		} catch (Exception e) {
 			Assert.fail("Test Fail");
 		}
 
+	}
+
+	private Path getResourcePath(String resourceName) {
+		Path path = null;
+		try {
+			ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+			URL url = classLoader.getResource(resourceName);
+			path = Paths.get(url.toURI());
+		} catch (URISyntaxException e) {
+			Assert.fail("Test Fail");
+			e.printStackTrace();
+		}
+		return path;
 	}
 
 }
